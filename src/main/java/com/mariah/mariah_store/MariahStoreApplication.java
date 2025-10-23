0f8c6a8b7b04cc6a524c3d1056094d4589d25aa5
Pedro Luiz de Souza Ellero <pedro_11_1@hotmail.com>
2025-10-22 21:29:11 -0300

    Inicializa projeto Spring Boot MariahStore

---
+++ src/main/java/com/mariah/mariah_store/MariahStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mariah.mariah_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariahStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MariahStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mariah/mariah_store/MariahStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariah.mariah_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MariahStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
